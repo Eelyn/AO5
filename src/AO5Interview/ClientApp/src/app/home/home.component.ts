d2ba24a94af1e10e3fd50932d739824c0307e20e
Eelyn Lim <eelyn_lim@hotmail.com>
2019-08-10 19:52:32 +1000

    simple UI of list of items

--- src/AO5Interview/ClientApp/src/app/home/home.component.ts
+++ src/AO5Interview/ClientApp/src/app/home/home.component.ts
@@ -1,8 +1,49 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
 })
-export class HomeComponent {
+
+export class HomeComponent implements OnInit {
+
+  public total: number;
+
+  constructor() {}
+
+  ngOnInit(): void {
+    this.total=0;
+  }
+
+  addBook() {
+    this.total++;
+  }
+
+  addMusic() {
+    this.total++;
+  }
+
+  addChoc() {
+    this.total++;
+  }
+
+  addImpChoc() {
+    this.total++;
+  }
+
+  addImpPerfume() {
+    this.total++;
+  }
+
+  addPerfume() {
+    this.total++;
+  }
+
+  addPill() {
+    this.total++;
+  }
+
+  submit() {
+
+  }
 }
