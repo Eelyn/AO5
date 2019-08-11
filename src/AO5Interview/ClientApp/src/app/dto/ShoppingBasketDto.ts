c231790df6546447c430865ccd95273e527b3b09
Eelyn Lim <eelyn_lim@hotmail.com>
2019-08-11 21:46:00 +1000

    new changes -- error msg to be fixed

---
+++ src/AO5Interview/ClientApp/src/app/dto/ShoppingBasketDto.ts
@@ -0,0 +1,4 @@
+export class ShoppingBasketDto {
+    ItemName: string;
+    Price: number;
+}
--- src/AO5Interview/ClientApp/src/app/home/home.component.ts
+++ src/AO5Interview/ClientApp/src/app/home/home.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { ShoppingBasketDto } from '../dto/ShoppingBasketDto';
 
 @Component({
   selector: 'app-home',
@@ -8,13 +9,21 @@ import { Component, OnInit } from '@angular/core';
 export class HomeComponent implements OnInit {
 
   public total: number;
+  item: string;
+  amount: number;
+  shoppingBasket: Array<ShoppingBasketDto>;
 
   constructor() {}
 
   ngOnInit(): void {
     this.total=0;
+    this.shoppingBasket = [];
+    let number = 0.86;
+    let result = (Math.ceil(number*20)/20).toFixed(2);
+    console.log(result);
   }
 
+  /*
   addBook() {
     this.total++;
   }
@@ -42,8 +51,20 @@ export class HomeComponent implements OnInit {
   addPill() {
     this.total++;
   }
-
+*/
   submit() {
+    if((this.amount != null) && (this.item != null)) {
+      let newAddition = new ShoppingBasketDto();
+      newAddition.ItemName = this.item;
+      newAddition.Price = this.amount;
+      this.shoppingBasket.push(newAddition);
 
+      let x = this.shoppingBasket[1].ItemName;
+      let y = this.shoppingBasket[1].Price;
+      console.log(x + ' ' + y);
+      this.total++;
+      this.item = '';
+      this.amount = null;
+    }
   }
 }
